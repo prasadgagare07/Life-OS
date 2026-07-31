5583ae764625eae669c9107cb678d8a84f075136
Prasad Gagare <prasadgagare91@gmail.com>
2026-08-01 01:34:51 +0530

    Update daily-standards.js

--- frontend/js/daily-standards.js
+++ frontend/js/daily-standards.js
@@ -1,3 +1,4 @@
+alert('history: ' + localStorage.getItem('lifeos_history') + '\n\ndate: ' + localStorage.getItem('lifeos_habits_date') + '\n\nlocked: ' + localStorage.getItem('lifeos_habits_locked'));
 // =======================================
 // LifeOS Daily Standards
 // Part 1
