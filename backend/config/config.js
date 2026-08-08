683106116218ccb8e9dc2b7e5461bce05f49170a
Prasad Gagare <prasadgagare91@gmail.com>
2026-08-08 16:07:30 +0530

    Remove passcodeHash from config

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,5 +4,4 @@ module.exports = {
   port: process.env.PORT || 5000,
   jwtSecret: process.env.JWT_SECRET,
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || '7d',
-  passcodeHash: process.env.PASSCODE_HASH,
 };
