9c129c11ddb0dc5bfca79666edf0414ef1f7d30a
Prasad Gagare <prasadgagare91@gmail.com>
2026-08-17 00:52:04 +0530

    Add betterme routes for habits and list items

---
+++ backend/routes/betterme.routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/betterme.controller');
+
+
+// Habits + month view
+router.get('/month', controller.getMonth);
+
+router.post('/habits', controller.addHabit);
+router.put('/habits/:id', controller.renameHabit);
+router.put('/habits/:id/position', controller.reorderHabit);
+router.delete('/habits/:id', controller.deleteHabit);
+
+router.post('/completion', controller.setCompletion);
+
+
+// List items (Learn / Master / Character)
+router.get('/list/:category', controller.getListItems);
+router.post('/list/:category', controller.addListItem);
+router.put('/list-item/:id', controller.renameListItem);
+router.put('/list-item/:id/toggle', controller.toggleListItem);
+router.delete('/list-item/:id', controller.deleteListItem);
+
+
+module.exports = router;
